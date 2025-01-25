8876cefcf7e2d725886201489f9334208fcdb067
walkercito <yoanky2005@gmail.com>
2025-01-24 22:26:13 -0500

    removed the timer

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -16,9 +16,7 @@ var facing_right: bool = true
 var can_air_dash: bool = true
 var dash_direction: int = 1
 var has_double_jump: bool = true
-var current_speed = WALK_SPEED
-var can_move: bool = true
-var land_lock: bool = false  # Nueva variable para control de aterrizaje
+var current_speed = WALK_SPEED 
 
 @onready var animationController = $sprite
 
@@ -27,7 +25,6 @@ func _physics_process(delta):
 	
 	current_speed = RUN_SPEED if Input.is_action_pressed("run") else WALK_SPEED
 	
-	# Actualizar animaciones después del movimiento
 	update_animations()
 	
 	if Input.is_action_just_pressed("dash") and (is_on_floor() or can_air_dash):
@@ -50,10 +47,7 @@ func _physics_process(delta):
 			velocity.x = sign(velocity.x) * WALK_SPEED
 		return
 	
-	var input = 0.0
-	if can_move:
-		input = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	
+	var input = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	if input != 0:
 		var accel = ACCELERATION if is_on_floor() else AIR_ACCELERATION
 		velocity.x = move_toward(velocity.x, input * current_speed, accel * delta)
@@ -64,6 +58,7 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 
+	
 	if is_on_floor():
 		has_double_jump = true
 		if Input.is_action_just_pressed("jump"):
@@ -75,27 +70,23 @@ func _physics_process(delta):
 	
 	move_and_slide()
 	
-	# Manejar bloqueo de animación al aterrizar
 	if not was_on_floor and is_on_floor():
 		can_air_dash = true
-		land_lock = true
-		await get_tree().physics_frame  # Esperar un frame físico
-		land_lock = false
+
 
 func update_animations() -> void:
 	animationController.flip_h = not facing_right
 
-	if is_dashing:
+	if (is_dashing):
 		animationController.play("dash")
-	elif not is_on_floor():
+	elif (not is_on_floor()):
 		if velocity.y < 0:
 			animationController.play("jump")
 		else:
 			animationController.play("fall")
+
 	else:
-		if land_lock:  # Forzar animación de idle durante el aterrizaje
-			animationController.play("idle")
-		elif abs(velocity.x) > 10:
+		if (abs(velocity.x) > 10):
 			animationController.play("run" if current_speed == RUN_SPEED else "walk")
 		else:
 			animationController.play("idle")
